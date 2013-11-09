111cf55619e2c227dae9d795e999474ee394d09e
Ryan Montgomery <rmontgomery429@gmail.com>
2013-11-08 19:26:56 -0500

    We need a secret_key_base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,3 @@
+# config/initializers/secret_token.rb
+CampBlodgett::Application.config.secret_token = 'existing secret token'
+CampBlodgett::Application.config.secret_key_base = 'new secret key base'
