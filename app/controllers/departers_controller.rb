1a8d720f05cd785f3fb8bac180b76c82a6fa44a7
Jason Swett <jason@electricsasquatch.com>
2013-11-10 12:16:11 -0500

    Fill in Departure Doc columns, order by name

--- app/controllers/departers_controller.rb
+++ app/controllers/departers_controller.rb
@@ -1,5 +1,5 @@
 class DepartersController < ApplicationController
   def index
-    @departers = CampSession.find(params[:camp_session_id]).camper_registrations
+    @departers = CampSession.find(params[:camp_session_id]).camper_registrations.order("last_name, first_name")
   end
 end
